14a4f95c6df3a46a94b73e037b4b19ff46f39c5e
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-10 12:27:31 +0200

    mapping the pizza info

--- src/components/PizzaList/PizzaList.js
+++ src/components/PizzaList/PizzaList.js
@@ -13,7 +13,23 @@ const PizzaList = () => {
       <p>
         Welcome back, <strong>{user.name}</strong>! There are
       </p>
-      <p>{pizzas.length} pizzas in total </p>
+      <p>
+        <strong>{pizzas.length} </strong>pizzas available
+      </p>
+      {/* renders the pizzas, this info comes from de slice, the hard coded data*/}
+      <ul>
+        {/* pizzas from de slice, do a map, and add a prop that can be any name */}
+        <h2>Here is the list of Pizzas</h2>
+        {pizzas.map((pizza) => (
+          <li key={pizza.id}>
+            <h4>{pizza.name}</h4>
+            <p>{pizza.description}</p>
+            <span>
+              Ordered: <strong>{pizza.bought}</strong> times!
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
