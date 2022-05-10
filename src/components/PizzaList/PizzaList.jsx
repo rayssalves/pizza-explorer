4d86249aeea68e0785a1b85512da017f288d6ac0
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-10 21:05:32 +0200

    adding style

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,3 +1,4 @@
+import "../../style.scss";
 import { useSelector, useDispatch} from "react-redux";
 import { selectUser } from "../../store/user/selector";
 //here import the function that was created in the selector
@@ -10,8 +11,9 @@ const PizzaList = () => {
   const dispatch = useDispatch();
   //here I do a const pizzas that is the name that is in the slice, doing a useSelector
   const pizzas = useSelector(listPizza);
+  
   return (
-    <div>
+    <div className="pizza-list">
       <h1>Pizza Explorer</h1>
       <p>
         Welcome back, <strong>{user.name}</strong>! There are
@@ -20,20 +22,29 @@ const PizzaList = () => {
         <strong>{pizzas.length} </strong>pizzas available
       </p>
       {/* renders the pizzas, this info comes from de slice, the hard coded data*/}
-      <ul>
+      <ul className="pizzas">
         {/* pizzas from de slice, do a map, and add a prop that can be any name */}
-        <h2>Here is the list of Pizzas</h2>
         {pizzas.map((pizza) => (
-          <li key={pizza.id}>
+          <li
+            key={pizza.id}
+            className="pizza"
+            style={{ backgroundImage: `url(${pizza.image})` }}
+          >
             <h4>{pizza.name}</h4>
-            <p>{pizza.description}</p>
-            {/* dispatch logic that was made in the user */}
-            <button onClick={() => dispatch(toggleFavorites(pizza.id))}>
+            <button
+              onClick={() => dispatch(toggleFavorites(pizza.id))}
+              className={`fav-toggle ${
+                user.favorites.includes(pizza.id) ? "fav" : ""
+              }`}
+            >
               {user.favorites.includes(pizza.id) ? "♥" : "♡"}
             </button>
-            <span>
-              Ordered: <strong>{pizza.bought}</strong> times!
-            </span>
+            <div className="overlay">
+              <p>{pizza.description}</p>
+              <span>
+                Bought: <strong>{pizza.bought}</strong> times!
+              </span>
+            </div>
           </li>
         ))}
       </ul>
